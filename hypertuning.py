beae644fa714c9f0b6a42e5bb4ef9b73e147d5c7
npanczyk <npanczyk@umich.edu>
2025-02-07 09:46:02 -0500

    fixed typo ==

--- hypertuning.py
+++ hypertuning.py
@@ -134,7 +134,7 @@ def set_space():
     Returns:
         dict: Dictionary with necessary hyperparameter spaces for hyperopt
     """
-    space = = {
+    space = {
         "depth": hp.choice("depth", [1, 2, 3, 4]),
         "grid": hp.choice("grid", [4, 5, 6, 7, 8, 9, 10]),
         "k": hp.choice("k", [2, 3, 4, 5, 6, 7, 8]),
